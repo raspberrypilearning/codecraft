a8916b56d11d693b479eb609666cbc002c37a5bc
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2019-11-27 11:49:48 +0000

    New translations variables.py (French)

--- fr-FR/resources/variables.py
+++ fr-FR/resources/variables.py
@@ -1,75 +1,75 @@
+#!/bin/python3
 
+#Variables du jeu qui peuvent être modifiées !
 
+#couleur de fond du jeu.
+COULEURDEFOND = 'white'
 
+#variables de la carte.
+MAXIMUMTUILES = 20
+LARGEURCARTE = 10
+HAUTEURCARTE = 10
 
+#Variales qui représentent les différentes ressources.
+TERRE = 0
+HERBE = 1
+EAU = 2
+BRIQUE = 3
 
+#liste de toutes les ressources du jeu.
+ressources = [TERRE,HERBE,EAU,BRIQUE]
 
+#noms des ressources.
+noms = {
+  TERRE : 'terre',
+  HERBE : 'herbe',
+  EAU : 'eau',
+  BRIQUE : 'brique'
+}
 
+#dictionnaire liant les ressources aux images.
+textures = {
+  TERRE : 'dirt.gif',
+  HERBE : 'grass.gif',
+  EAU : 'water.gif',
+  BRIQUE : 'brick.gif'
+}
 
+#nombre de chaque ressource que possède le joueur.
+inventaire = {
+  TERRE : 10,
+  HERBE : 10,
+  EAU : 10,
+  BRIQUE : 0
+}
 
+#image du joueur.
+imgJoueur = 'player.gif'
 
+#position du joueur.
+xJoueur = 0
+yJoueur = 0
 
+#règles pour fabriquer des nouvelles ressources.
+fabrication = {
+  BRIQUE : { EAU: 1, TERRE : 2 }
+}
 
+#touches pour placer des ressources.
+touchesPlacement = {
+  TERRE : '1',
+  HERBE : '2',
+  EAU : '3',
+  BRIQUE : '4'
+}
 
+#touches pour fabriquer les blocs.
+touchesFabrication = {
+  BRIQUE : 'r'
+}
 
-
-
-
-
-
-
-
-
-
-  
-  
-  
-  
-
-
-
-
-  
-  
-  
-  
-
-
-
-
-  
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-  
-  
-  
-  
-
-
-
-
-  
-
-
-
-
-  
-  
-
+#instructions de jeu affichées.
+instructions = [
+  'Instructions:',
+  'Utilise ZQSD pour te déplacer'
+]
